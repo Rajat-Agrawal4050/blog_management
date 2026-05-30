50baa256c56c5e0e98fcb4261442e445f45fd99a
Rajat Agrawal <130221448+Rajat-Agrawal4050@users.noreply.github.com>
2026-05-30 16:26:53 +0530

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -19,7 +19,7 @@ class Comment extends Model
     // ─── Relationships ────────────────────────────────────
     public function post(): BelongsTo
     {
-        return $this->belongsTo(Post::class);
+        return $this->belongsTo(Post::class,'posts','id');
     }
 
     public function user(): BelongsTo
